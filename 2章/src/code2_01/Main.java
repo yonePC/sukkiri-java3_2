466be5fa508ff2e59150db74b2891f0a900014f2
yonePC <ys.mailaddress0929@gmail.com>
2021-12-12 06:52:42 +0900

    2-1

---
+++ 2章/src/code2_01/Main.java
@@ -0,0 +1,14 @@
+package code2_01;
+
+public class Main {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		
+		a = 10;
+		b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
