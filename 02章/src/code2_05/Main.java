ea413d1c86cb1a573f462557edcc342214698c44
yonePC <ys.mailaddress0929@gmail.com>
2021-12-13 17:11:24 +0900

    2-5

---
+++ 02章/src/code2_05/Main.java
@@ -0,0 +1,11 @@
+package code2_05;
+
+public class Main {
+	public static void main(String[] args) {
+		float f = 3;
+		double d = f;
+		System.out.println(f);
+		System.out.println(d);
+	}
+
+}
---
+++ 02章/src/code2_07/Main.java
@@ -0,0 +1,9 @@
+package code2_07;
+
+public class Main {
+	public static void main(String[] args) {
+		int age = (int)3.2;
+		System.out.println(age);
+	}
+
+}
