1ce1e045be269b0cd0c6fa1e2c32a314a7afc7ad
yonePC <ys.mailaddress0929@gmail.com>
2021-12-21 13:02:10 +0900

    2-6

---
+++ 02章/src/code2_12/Main.java
@@ -0,0 +1,11 @@
+package code2_12;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		int m = Math.max(a, b);
+		System.out.println("比較実験：" + a + "と" + b + "とで大きいほうは" + m);
+	}
+
+}
