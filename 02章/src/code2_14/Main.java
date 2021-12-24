494846aa065996269a502ffabb90c67e59014318
yonePC <ys.mailaddress0929@gmail.com>
2021-12-24 09:32:48 +0900

    2-6-5

---
+++ 02章/src/code2_14/Main.java
@@ -0,0 +1,9 @@
+package code2_14;
+
+public class Main {
+	public static void main(String[] args) {
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたはたぶん" + r + "歳ですね？");
+	}
+
+}
