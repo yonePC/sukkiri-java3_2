ebbd344543880012930d59022d1c8c5beb66adcc
yonePC <ys.mailaddress0929@gmail.com>
2021-12-26 13:48:50 +0900

    3-1-2

---
+++ 03章/src/code3_01/Main.java
@@ -0,0 +1,14 @@
+package code3_01;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("選択をします");
+			System.out.println("散歩に行きます");
+		} else { 
+			System.out.println("DVDを見ます");
+		}
+	}
+
+}
