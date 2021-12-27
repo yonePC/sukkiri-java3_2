d5ac48081584142ed686be911dfaf0502f0e1997
yonePC <ys.mailaddress0929@gmail.com>
2021-12-27 10:42:38 +0900

    3-1-3

---
+++ 03章/src/code3_02/Main.java
@@ -0,0 +1,12 @@
+package code3_02;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean doorClose = false;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		} 
+	}
+
+}
